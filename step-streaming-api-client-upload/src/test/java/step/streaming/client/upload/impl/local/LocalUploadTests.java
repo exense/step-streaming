e17d1af59b9921522f2354137040b5c691d51901
Christoph Langguth <christoph.langguth@exense.ch>
2025-08-19 14:59:47 +0200

    SED-4178 New UnitTest

--- step-streaming-api-client-upload/src/test/java/step/streaming/client/upload/impl/local/LocalUploadTests.java
+++ step-streaming-api-client-upload/src/test/java/step/streaming/client/upload/impl/local/LocalUploadTests.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.time.Duration;
+import java.util.Objects;
 
 public class LocalUploadTests {
 
@@ -32,7 +33,32 @@ public class LocalUploadTests {
         }
     }
 
-    private class LineProducer extends Thread{
+    @Test
+    public void testDirectoryUpload() throws Exception {
+        File tmpDir = Files.createTempDirectory("streaming-upload-test-").toFile();
+        StreamingUploads uploads = new StreamingUploads(new LocalDirectoryBackedStreamingUploadProvider(tmpDir));
+        File liveFile = Files.createTempFile("streaming-upload-test-", ".txt").toFile();
+        LineProducer producer = new LineProducer(liveFile, 10, 200);
+        try {
+            StreamingUpload upload = uploads.startTextFileUpload(liveFile);
+            producer.start();
+            producer.join();
+            Assert.assertEquals(new StreamingResourceStatus(StreamingResourceTransferStatus.COMPLETED, 141, 10L), upload.complete(Duration.ofSeconds(1)));
+            Assert.assertEquals(1, Objects.requireNonNull(tmpDir.listFiles()).length);
+            File uploaded = Objects.requireNonNull(tmpDir.listFiles())[0];
+            Assert.assertTrue(uploaded.getName().endsWith(liveFile.getName()));
+            Assert.assertTrue(uploaded.delete());
+        } finally {
+            try {
+                producer.stop();
+            } catch (Exception ignored) {}
+            Files.deleteIfExists(liveFile.toPath());
+            Files.deleteIfExists(tmpDir.toPath());
+        }
+    }
+
+
+    private static class LineProducer extends Thread{
         private final File outputFile;
         private final int numberOfLines;
         private final long sleepBetween;
